d9015264afe183b3e33fa092bb92e6232e969e66
romeoteto <romeo.kamdem@objective-partner.com>
2025-05-01 18:14:18 +0200

    oc-15 automatisiertes Deployment des Operaide UI Clickdummies

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   server: {
     host: "0.0.0.0", // 👈 This is the key part
   },
+  base: '/operaide-ui-clickdummy/',
 });
