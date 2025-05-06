0999bf4394c94a34721e0862e13c4dc293eeefb6
romeoteto <romeo.kamdem@objective-partner.com>
2025-05-06 10:27:08 +0200

    Updates

---
+++ assets/stateDiagram-v2-YXO3MK2T-DJM24qyJ.js
@@ -0,0 +1,1 @@
+import{s as t,b as a,a as r,S as s}from"./chunk-AEK57VVT-DLxrRPSw.js";import{_ as i}from"./index-oyfmqkFw.js";import"./chunk-RZ5BOZE2-BLGcPRA1.js";var _={parser:r,get db(){return new s(2)},renderer:a,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{_ as diagram};
